5587e6f9b2f9ad7ad4cbe97c0336db32b606889c
rspraneeth <rspraneeth.rsp@gmail.com>
2023-11-09 14:53:10 -0600

    collection classes

--- Collection/src/main/java/LaunchFFFS.java
+++ Collection/src/main/java/LaunchFFFS.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class LaunchFFFS { // fail fast, fail safe
     public static void main(String[] args) {
@@ -8,15 +9,29 @@ public class LaunchFFFS { // fail fast, fail safe
         list.add(20);
         list.add(30);
         list.add(40);
+ /*
 //        for (int i = 0; i<list.size(); i++) {
 //            System.out.println(list.get(i));
 //            list.add(44); //modifying while accessing - concurrent modification
 //        }
 
-        Iterator itr = list.iterator();
-        while (itr.hasNext()){
-            System.out.println(itr.next()+" ");
-            list.add(25);
+//        Iterator itr = list.iterator();
+//        while (itr.hasNext()){
+//            System.out.println(itr.next()+" ");
+//            list.add(25); // ConcurrentModificationException , ***"Fail Fast"***
+//        }*/
+
+        CopyOnWriteArrayList list1 = new CopyOnWriteArrayList();
+        list1.add(10);
+        list1.add(20);
+        list1.add(30);
+        list1.add(40);
+
+        Iterator itr1 = list1.iterator();
+        while (itr1.hasNext()){
+            System.out.println(itr1.next()+" ");
+            list1.add(25); // Failing ConcurrentModificationException safely , ***"Fail Safe"***
         }
+
     }
 }
