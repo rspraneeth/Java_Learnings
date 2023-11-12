9e3a746cd599a6f2c832bd6b99a175221187f56d
rspraneeth <rspraneeth.rsp@gmail.com>
2023-11-12 13:42:35 -0600

    Stream API introduction, Intermediate & Terminal operations

---
+++ StreamAPIDemo/src/main/java/StreamsDemo.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+
+        Predicate<Integer> predicate = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer i) {
+                System.out.println("in test: Predicate"+i);
+                return i % 2 == 0;
+            }
+        };
+
+        Function<Integer, Integer> function = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer num) {
+                System.out.println("in apply: Function"+num);
+                return num*2; // the same(Function) is passed directly to map()
+            }
+        };
+
+        BinaryOperator<Integer> bi = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer c, Integer e) {
+                System.out.println("in apply: BinaryOperator"+c+" "+e);
+                return c+e; //the same(BinaryOperator) is passed directly to reduce()
+            }
+        };
+
+        List<Integer> nums = Arrays.asList(4, 2, 5, 7, 8, 3, 1, 6);
+
+        Integer sum = nums.stream()
+                .filter(predicate)
+                .map(function)
+                .reduce(0, bi);
+
+        System.out.println(sum);
+
+        System.out.println(nums.stream().filter(i->i%2==0).map(i->i*2).reduce(0, (Integer s, Integer i) ->  s+i));
+
+        System.out.println("*********************************************************");
+
+        System.out.println("Here, filter and map are Intermediate operations(Lazy), reduce is Terminal operation(Eager)");
+
+        System.out.println("Intermediate operations return a new stream. They are always lazy; \n" +
+                "executing an intermediate operation such as filter() does not actually perform \n" +
+                "any filtering, but instead creates a new stream that, when traversed, \n" +
+                "contains the elements of the initial stream that match the given predicate. \n" +
+                "Traversal of the pipeline source does not begin until the terminal operation \n" +
+                "of the pipeline is executed");
+        nums.stream()
+                .filter(predicate)
+                .map(function);
+//                .reduce(0, bi);
+//        so if we comment the reduce function, then nothing will get executed because filter and map are
+//        intermediate or lazy operation, they execute only when terminal or eager operation gets executed.
+
+    }
+}
