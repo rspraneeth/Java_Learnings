e0e50859ce20ca16e9b7d3fde64c0b7bbbe4f1ad
rspraneeth <rspraneeth.rsp@gmail.com>
2023-11-16 19:22:16 -0600

    generics

---
+++ Generics/src/GenericsDemo.java
@@ -0,0 +1,47 @@
+interface IGen<T>{
+    void disp();
+
+}
+
+class Human{
+    public void sleep(){
+        System.out.println("Humans nee to sleep");
+    }
+}
+class Genr<T> implements IGen<String>{
+    T ref;
+
+    public  Genr(T ref){
+        this.ref = ref;
+    }
+
+//    public  Genr(){
+//    }
+
+    public  void disp(){
+        System.out.println("The type passed to generic<T> : "+ref.getClass().getName());
+    }
+
+    public T getRef() {
+        return ref;
+    }
+}
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        Genr<Integer> genr = new Genr<>(11);
+        Genr<String> genr1 = new Genr<>("String object text");
+//        Genr gen = new Genr(22); // Without ' <> ' gives warning
+        genr.disp();
+        System.out.println(genr.getRef());
+        genr1.disp();
+        System.out.println(genr1.getRef());
+
+        Genr<Human> genr2 = new Genr<>(new Human());
+        genr2.disp();
+        System.out.println(genr2.getRef());
+
+        IGen<String> g = new Genr<>("Java Data");
+        g.disp();
+    }
+}
